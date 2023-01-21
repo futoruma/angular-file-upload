de14520b1626c13b0e0d4f7dbb452a2b5a6de7f1
futoruma <evgeny.martov@gmail.com>
2023-01-21 21:26:45 +0900

    added css styles

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -3,26 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class UploadComponent implements OnInit {
   isHovering: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   toggleHover(event: boolean) {
     this.isHovering = event;
   }
 
-  startUpload(event: any) {
+  startUpload(event: any | FileList) {
+    console.log(event);
     const file = event.item(0);
-    if (file?.type.split('/')[0] === 'video') {
-      console.error('video files are not supported');
-    }
-    const path = `test/${new Date().getTime()}_${file?.name}`;
-  }
+    console.log(file);
 
+    // if (file?.type.split('/')[0] === 'video') {
+    //   console.error('video files are not supported');
+    // }
+
+    // const path = `test/${new Date().getTime()}_${file?.name}`;
+  }
 }
